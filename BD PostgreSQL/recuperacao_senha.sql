45e5868ce61ec7cab5a66a1a5a29856ce51774a4
Mateus Silva <mateusfogsilva@alunos.fho.edu.br>
2025-11-29 04:00:19 -0300

    BD_Limpo
    arquivos do BD desnecessário excluidos

--- BD PostgreSQL/recuperacao_senha.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE recuperacao_senha (
-    id SERIAL PRIMARY KEY,
-    usuario_id INTEGER NOT NULL REFERENCES usuarios(id),
-    token VARCHAR(64) NOT NULL UNIQUE,
-    data_expiracao TIMESTAMP NOT NULL,
-    usado BOOLEAN DEFAULT FALSE,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
